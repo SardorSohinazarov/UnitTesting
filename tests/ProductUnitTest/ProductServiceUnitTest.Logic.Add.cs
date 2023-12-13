fb58ed7599a26e07f849fac05d75b5e8d920c838
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-13 18:55:57 +0500

    Add project files.

---
+++ tests/ProductUnitTest/ProductServiceUnitTest.Logic.Add.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using ProductAPI.Entities;
+
+namespace ProductUnitTest
+{
+    public partial class ProductServiceUnitTest
+    {
+        [Fact]
+        public async Task ShouldAddProduct()
+        {
+            var inputProduct = new Product()
+            {
+                Id = 1,
+                Name = "TestProduct",
+            };
+            
+            var resultProduct = new Product()
+            {
+                Id = 1,
+                Name = "TestProduct",
+            };
+
+            _productRepositoryMock.Setup(x => x.InsertAsync(inputProduct))
+                .ReturnsAsync(resultProduct);
+
+            var outputProduct = await _productService.AddAsync(inputProduct);
+
+            Assert.Equal(resultProduct.Name, outputProduct.Name);
+            Assert.Equal(resultProduct.Id, outputProduct.Id);
+
+            _productRepositoryMock.Verify(x => x.InsertAsync(inputProduct),
+                Times.Once());
+
+            _productRepositoryMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ tests/ProductUnitTest/ProductServiceUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using ProductAPI.Repositories;
+using ProductAPI.Services;
+
+namespace ProductUnitTest
+{
+    public partial class ProductServiceUnitTest
+    {
+        private readonly IProductService _productService;
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+
+        public ProductServiceUnitTest()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _productService = new ProductService(_productRepositoryMock.Object);
+        }
+    }
+}
